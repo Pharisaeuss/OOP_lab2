6b858798167cf96cfdd427e7a269517de33c36da
Pharisaeuss <146725724+Pharisaeuss@users.noreply.github.com>
2023-12-08 11:05:36 +0200

    lab 2

---
+++ PlayerType.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGame
+{
+
+    interface ChangedRating_Type
+    {
+        int ChangedRating(bool isWin);
+    } 
+
+
+    class Warrior : ChangedRating_Type
+    {
+        public int ChangedRating(bool winORlose)
+        {
+            return winORlose ? 0 : 0;
+        }
+    }
+
+    class Assassin : ChangedRating_Type
+    {
+        public int ChangedRating(bool winORlose)
+        {
+            return winORlose ? 0 : -40; 
+        }
+    }
+
+    class HollowKnight : ChangedRating_Type
+    {
+        private int WinsInRow = 0;
+
+        public int ChangedRating(bool winORlose)
+        {
+            if (winORlose)
+            {
+                WinsInRow++;
+                return WinsInRow * 20; 
+            }
+            else
+            {
+                WinsInRow = 0; 
+                return -50;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace MyGame
+{
+
+    class Program
+    {
+        static void Main()
+        {
+
+            Game standardGame = GameFactory.CreateStandardGame();
+            Game trainingGame = GameFactory.CreateTrainingGame();
+            Game singlePlayerGame = GameFactory.CreateSinglePlayerGame();
+
+            GameAccount player1 = new GameAccount("Pipipypy", 1250, new Warrior());
+            GameAccount player2 = new GameAccount("Pog", 1600, new Assassin());
+            GameAccount player3 = new GameAccount("Frog", 1000, new HollowKnight());
+
+            player1.WinGame("Martins", standardGame);
+            player1.LoseGame("Kerbal", standardGame);
+            player1.WinGame("abc2008", standardGame);
+            player1.WinGame("help", trainingGame);
+            player1.WinGame("Botik", singlePlayerGame);
+
+
+            player2.WinGame("Obama", standardGame);
+            player2.LoseGame("Badin", standardGame);
+            player2.WinGame("Botik", singlePlayerGame);
+            player2.WinGame("Jerb", trainingGame);
+            player2.LoseGame("Boba", trainingGame);
+
+            player3.WinGame("Bob228", standardGame);
+            player3.WinGame("Botik", singlePlayerGame);
+            player3.WinGame("Kotik", singlePlayerGame);
+            player3.LoseGame("Cito", standardGame);
+            player3.WinGame("Pants", standardGame);
+
+            player1.GetStats();
+            player2.GetStats();
+            player3.GetStats();
+
+        }
+    }
+}
